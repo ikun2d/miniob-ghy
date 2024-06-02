e524478afebfc1a41da58bbde39189706141b661
ikun2d <163657135+ikun2d@users.noreply.github.com>
2024-06-02 16:54:56 +0800

    Update clog_reader_cmd.cpp

--- tools/clog_reader_cmd.cpp
+++ tools/clog_reader_cmd.cpp
@@ -8,9 +8,6 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
-//
-// Created by wangyunlai.wyl on 2023/06/07
-//
 
 #include <inttypes.h>
 
