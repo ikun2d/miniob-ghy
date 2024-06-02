5a840c53f8cc5c46bd226dff2a41cbfeb439dae9
ikun2d <163657135+ikun2d@users.noreply.github.com>
2024-06-02 16:52:53 +0800

    Update main.cpp

--- src/observer/main.cpp
+++ src/observer/main.cpp
@@ -8,12 +8,7 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
-// __CR__
 
-/*
- *  Created on: Mar 11, 2012
- *      Author: Longda Feng
- */
 
 #include <netinet/in.h>
 #include <unistd.h>
