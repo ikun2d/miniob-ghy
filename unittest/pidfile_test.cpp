305412cf004b25e8da10986a8f4523c8470c232a
ikun2d <163657135+ikun2d@users.noreply.github.com>
2024-06-02 16:56:15 +0800

    Update pidfile_test.cpp

--- unittest/pidfile_test.cpp
+++ unittest/pidfile_test.cpp
@@ -8,9 +8,7 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
-//
-// Created by Longda on 2021/4/16.
-//
+
 #include <unistd.h>
 
 #include "gtest/gtest.h"
