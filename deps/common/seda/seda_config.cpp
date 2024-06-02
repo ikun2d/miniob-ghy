e5bc55338ebdda472b8d3f41f6bbe6230375ab07
ikun2d <163657135+ikun2d@users.noreply.github.com>
2024-06-02 16:16:07 +0800

    Update seda_config.cpp

--- deps/common/seda/seda_config.cpp
+++ deps/common/seda/seda_config.cpp
@@ -8,9 +8,6 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
-//
-// Created by Longda on 2010
-//
 #include "common/seda/seda_config.h"
 
 #include <sys/stat.h>
